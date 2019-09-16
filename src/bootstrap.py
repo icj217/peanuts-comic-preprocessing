0ca73606f696bf2917d049679259a58fc37dbe5e
Craig Burdulis <craig.burdulis@jefferson.edu>
2019-09-16 14:51:41 -0400

    Changed config loading to use local.ini file when present

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -1,6 +1,8 @@
 from utils.s3 import get_client
 from utils.config import get_config
 import zipfile
+import logging
+
 # Updates local environment with S3 data (which we assume to be the latest)
 s3 = get_client("s3")
 config = get_config()
@@ -14,15 +16,19 @@ local_cropped_dir = config["directories"]["cropped_images"]
 
 # Download Raw Images
 local_raw_images_zip = "zippedRawImagesFromS3.zip"
+logging.info("Downloading %s to %s", s3_raw_images, local_zip_dir + local_raw_images_zip)
 s3.download_file(s3_bucket_name, s3_raw_images, local_zip_dir + local_raw_images_zip)
 
 # Download Raw Images
 local_cropped_images_zip = "zippedCroppedImagesFromS3.zip"
+logging.info("Downloading %s to %s", s3_cropped_images, local_zip_dir + local_cropped_images_zip)
 s3.download_file(s3_bucket_name, s3_cropped_images, local_zip_dir + local_cropped_images_zip)
 
 # Unzip Files
-with zipfile.ZipFile(local_raw_images_zip, 'r') as zip:
+logging.info("Unzipping files")
+with zipfile.ZipFile(local_zip_dir + local_raw_images_zip, 'r') as zip:
   zip.extractall(local_raw_dir)
 
-with zipfile.ZipFile(local_cropped_images_zip, 'r') as zip:
+with zipfile.ZipFile(local_zip_dir + local_cropped_images_zip, 'r') as zip:
   zip.extractall(local_raw_dir)
+logging.info("Done bootstrapping!")
--- src/utils/config.py
+++ src/utils/config.py
@@ -2,5 +2,6 @@ import configparser
 
 def get_config():
   config = configparser.ConfigParser()
-  config.read("config.ini")
+  config.read("config/default.ini")
+  config.read("config/local.ini")
   return config
