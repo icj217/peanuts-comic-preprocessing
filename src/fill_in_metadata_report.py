98e4d7028c0913d28c59eb4a5e37e9369965e37d
Craig Burdulis <craigburdulis@CJ-LT.local>
2019-09-30 07:28:54 -0400

    Added fixing of page to fill in script

--- src/fill_in_metadata_report.py
+++ src/fill_in_metadata_report.py
@@ -5,17 +5,18 @@ import re
 
 report_file_name = sys.argv[1]
 
-report_updated_file_name = report_file_name.split('.')[0] + '_updated.' + report_file_name.split('.')[1]
+report_updated_file_name = report_file_name.split('.')[0] + '_updated.csv'
 
 with open(report_file_name, newline='') as report, open(report_updated_file_name, mode='w+', newline='') as output:
     rows = csv.reader(report, delimiter='\t', quotechar='"')
-    writer = csv.writer(output)
+    writer = csv.writer(output, dialect='excel')
 
     keep_going = True
     previous_row = None
 
     year_idx = 4
     month_idx = 3
+    page_idx = 5
 
     while keep_going:
         try:
@@ -26,11 +27,20 @@ with open(report_file_name, newline='') as report, open(report_updated_file_name
                 print("This is the first row. We're gonna move to next one so we have a previous row to work with")
                 writer.writerow(this_row)
             else:
+                # Fix Year
                 if this_row[year_idx] == 'None' and previous_row[year_idx] != 'None':
                     this_row[year_idx] = previous_row[year_idx]
-                if this_row[month_idx] == 'None' and previous_row[month_idx] != 'None':
+                # Fix Month
+                if previous_row[month_idx] == "['DECEMBER', 'JANUARY']":
+                    # Incrementing year
+                    this_row[year_idx] = int(this_row[year_idx]) + 1
+                    this_row[month_idx] = ['JANUARY']
+                elif this_row[month_idx] == 'None' and previous_row[month_idx] != 'None':
                     months = re.findall(r"'(.*?)'", str(previous_row[month_idx])) or []
                     this_row[month_idx] = [months[1]] if len(months) > 1 else months
+                # Fix Page
+                if this_row[page_idx] == 'None' and previous_row[page_idx] != 'None' and this_row[1] == previous_row[1]:
+                    this_row[page_idx] = int(previous_row[page_idx]) + 1
                 writer.writerow(this_row)
             # When done with this row
             previous_row = this_row
