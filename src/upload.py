4b94557ae93ad8739b9b2ef4865e281efb3cd441
Craig Burdulis <craig.burdulis@jefferson.edu>
2019-09-30 15:57:58 -0400

    Initial logic for upload script that uploads files to S3 and adds items to DDB table

--- src/upload.py
+++ src/upload.py
@@ -2,6 +2,10 @@ import glob
 import uuid
 import boto3
 import logging
+import csv
+import sys
+import re
+import arrow
 from utils.config import get_config
 
 config = get_config()
@@ -12,23 +16,9 @@ s3_bucket = config["s3"]["bucket_name"]
 ddb = boto3.client('dynamodb')
 s3 = boto3.client('s3')
 
-def put_item(comic_id, book_id, file_name, comic_type):
-  item = {
-    'comic_id': {
-      'S': comic_id
-    },
-    'book_id': {
-      'S': book_id
-    },
-    'file_name': {
-      'S': file_name
-    },
-    'comic_type': {
-      'S': comic_type
-    }
-  }
+def put_item(item):
   try:
-    return ddb.put_item(TableName=ddb_table, Item=item, ConditionalExpression="attribute_not_exists(comic_id)")
+    return ddb.put_item(TableName=ddb_table, Item=item, ConditionalExpression="attribute_not_exists(comic_id) AND attribute_not_exists(display_date)")
   except Exception as e:
     logging.error("Failed to put item %s: %s", comic_id, str(e))
 
@@ -45,17 +35,54 @@ def upload_file(file_name, bucket, object_name=None):
         return False
     return True
 
-files = glob.glob(source_directory)
-print('files:', files)
-for file in files:
+report_file_name = sys.argv[1]
+
+with open(report_file_name, newline='') as report:
+  # Load metadata into dict
+  reader = csv.DictReader(report, delimiter=',', quotechar='"')
+  metadata = { rows[0].split('/')[-1]:rows for rows in reader }
+  # Define start date
+  time = arrow.utcnow()
+  # Iterate through files
+  files = glob.glob(source_directory)
+  for file in files:
     # Get variables
     file_name = file.split('/')[-1]
-    file_parts = file_name[file_name.startswith('tpb-') and len('tpb-'):].split('_')
-    book_id = file_parts[0]
+    file_ext = file.split('.')[-1]
+    file_parts = file_name.split('_')
+    original_file_name = file_parts[0] + '.' + file_ext
+    book_id = file_parts[0].replace('tpb-', '')
     comic_type = file_parts[2]
+    page_metadata = metadata.get(file_name)
+    book_num = page_metadata['book']
+    page_num = page_metadata['page2']
+    pub_year = page_metadata['year']
+    pub_month = re.findall(r"'(.*?)'", str(page_metadata['months'])) or []
+    display_date = time.date().isoformat()
+    is_bt_months = len(pub_month) > 1
     # Generate UUID
     comic_id = uuid.uuid4()
-    # Insert item into DDB
-    put_item(comic_id, book_id, file_name, comic_type)
-    # Upload image to S3 under UUID name
-    upload_file(file, s3_bucket, comic_id)
+    # Generate URL (should we use bucket name or something more stable?)
+    url = 'https://{}.s3.amazonaws.com/strips/{}'.format(s3_bucket, comic_id)
+    # Build Item
+    item = {}
+    item['comic_id'] = { 'S': comic_id }
+    item['url'] = { 'S': url }
+    item['comic_type'] = { 'S': comic_type }
+    item['book_num'] = { 'N': page_metadata }
+    item['page_num'] = { 'N': page_metadata }
+    item['pub_year'] = { 'N': page_metadata }
+    item['pub_month'] = { 'N': page_metadata }
+    item['pub_day'] = { 'NULL': True }
+    item['display_date'] = { 'S': display_date }
+    item['original_file_name'] = { 'S': file_name }
+    item['is_bt_months'] = { 'BOOL': is_bt_months }
+    try:
+      # Insert item into DDB
+      put_item(item)
+      # Upload image to S3 under UUID name
+      upload_file(file, s3_bucket, comic_id)
+      # Increment day
+      time.shift(days=1)
+    except Exception as e:
+      logging.error("Failed to upload/insert file: %s", str(e))
