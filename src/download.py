714f990e99ee04adff1a82511e1322cef5990a15
icj217 <icj217@gmail.com>
2019-11-04 15:12:53 -0500

    Removed files

--- src/download.py
+++ src/download.py
@@ -1,34 +1,7 @@
 import urllib.request
 from utils.config import get_config
 
-files = ["https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-1.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-2.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-3.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-4.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-5.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-6.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-7.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-8.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-9.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-10.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-11.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-12.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-13.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-14.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-15.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-16.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-17.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-18.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-19.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-20.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-21.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-22.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-23.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-24.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-25.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-26-part-1.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-26-part-2.zip",
-"https://www.zipcomic.com/storage/s4/the-complete-peanuts/tpb-26-part-3.zip"]
+files = []
 
 config = get_config()
 zip_directory = config["directories"]["zipped_images"]
